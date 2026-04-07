029b1f80e4aa46a8eccfb1c39b119b116d873d43
hardiycore <hardiycores@gmail.com>
2026-04-07 15:59:12 -0300

    Update TextSpace.jsx
    welcome

--- components/TextSpace.jsx
+++ components/TextSpace.jsx
@@ -4,8 +4,7 @@ import styles from '../styles/components/TextSpace.module.css';
 function TextSpace() {
   return (
     <div className={styles.textSpace}>
-      <div>Welcome to the digital art portfolio<br/>
-      of Alex Arroyo<span className={styles.blink}>...</span></div>
+      <div>welcome!<span className={styles.blink}>...</span></div>
     </div>
   );
 }
