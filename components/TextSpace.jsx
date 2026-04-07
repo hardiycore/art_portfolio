f40ba465fb1d5d4a33b864532819d6cd9d9f71fb
hardiycore <hardiycores@gmail.com>
2026-04-07 16:09:21 -0300

    Update TextSpace.jsx
    ...gone

--- components/TextSpace.jsx
+++ components/TextSpace.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/TextSpace.module.css';
 function TextSpace() {
   return (
     <div className={styles.textSpace}>
-      <div>welcome!<span className={styles.blink}>...</span></div>
+      <div>welcome!<span className={styles.blink}></span></div>
     </div>
   );
 }
